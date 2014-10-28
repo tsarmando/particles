ea8bfaffe34325d911e7e057fd833f6966d35c73
tsarmando <magicmonkey511@gmail.com>
2014-10-28 13:56:37 -0700

    Create particle.hpp

---
+++ particle.hpp
@@ -0,0 +1,40 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+#include <stdio.h>
+#include "vector.hpp"
+
+class partcl
+{
+	public:
+		
+		partcl(vectr u, vectr r, vectr v,vectr a);
+		
+		partcl(){};
+		//magnetization intensity particle diameter
+		double m, diam; 
+		//orientation of magnet, position vector, initial velocity
+		//acceleration
+		
+		vectr u, r, v, a;
+
+		
+		void positionSet(vectr a);
+		void orientationSet(vectr a);
+		void accelSet(vectr accel);
+		void velocitySet(vectr a);
+		vectr getOrientation();
+		vectr getAcceleration();
+		vectr getPosition();
+		vectr getVelocity();
+		void print_partcl(FILE *outfile, int i) const;
+		
+}; 
+
+//a particle in our system
+vectr ComputeDipole(partcl *a, partcl *b); 
+vectr torque(partcl *a, partcl *b);  
+void seperation(partcl *a, partcl *b); 
+partcl **InitCord(double lowx, double upx, double lowy, 
+	double upy, double lowz, double upz, int nPartcl);
+		
+#endif
